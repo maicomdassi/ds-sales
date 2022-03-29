51ffdefb361f13b852630e3e5fb3ed37cd972be9
Michael Zafanelli <maicomdassi@gmail.com>
2022-03-29 17:19:48 -0300

    add Filter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
+import Filter from './components/Filter';
 import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Header />
+
+      <div className="app-container">
+        <Filter />
+      </div>
     </>
   );
 }
